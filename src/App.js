284a6cd959e63cf5483cceeafb7cef3cb4c29eaa
Shubham Sharma <sharmashibu787@gmail.com>
2025-06-26 22:03:37 +0530

    Setup GitHub Pages deployment

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import { useEffect, useState } from "react";
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./Pages/Home";
 import Legal from "./Pages/Legal";
@@ -31,7 +31,7 @@ function App() {
   }
   return (
     <div className="App">
-      <Router basename="/Health-Plus">
+      <Router basename="/Healthcare">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/legal" element={<Legal />} />
